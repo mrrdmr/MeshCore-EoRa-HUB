38e1474620e6d58b65874e661e6cbe04fcf2641f
radmir <39234121+mrrdmr@users.noreply.github.com>
2026-07-04 21:43:02 +0300

    clean up

--- src/helpers/radiolib/CustomLR1121Wrapper.h
+++ src/helpers/radiolib/CustomLR1121Wrapper.h
@@ -6,70 +6,43 @@
 
 class CustomLR1121Wrapper : public RadioLibWrapper {
 public:
-  CustomLR1121Wrapper(CustomLR1121 &radio, mesh::MainBoard &board)
-      : RadioLibWrapper(radio, board) {}
+  CustomLR1121Wrapper(CustomLR1121& radio, mesh::MainBoard& board) : RadioLibWrapper(radio, board) { }
+  void doResetAGC() override { lr11x0ResetAGC((LR11x0 *)_radio, ((CustomLR1121 *)_radio)->getFreqMHz()); }
 
   void setParams(float freq, float bw, uint8_t sf, uint8_t cr) override {
-    CustomLR1121 *r = (CustomLR1121 *)_radio;
+    auto* r = (CustomLR1121*)_radio;
     r->setFrequency(freq);
-    r->setSpreadingFactor(sf);
     r->setBandwidth(bw);
+    r->setSpreadingFactor(sf);
     r->setCodingRate(cr);
     updatePreamble(sf);
   }
 
-  bool isReceivingPacket() override {
+  bool isReceivingPacket() override { 
     return ((CustomLR1121 *)_radio)->isReceiving();
   }
-
   float getCurrentRSSI() override {
     float rssi = -110;
     ((CustomLR1121 *)_radio)->getRssiInst(&rssi);
     return rssi;
   }
 
-  float getLastRSSI() const override {
-    return ((CustomLR1121 *)_radio)->getRSSI();
-  }
-
-  float getLastSNR() const override {
-    return ((CustomLR1121 *)_radio)->getSNR();
-  }
-
   uint8_t getSpreadingFactor() const override {
-    return ((CustomLR1121 *)_radio)->getSpreadingFactor();
-  }
-
-  float packetScore(float snr, int packet_len) override {
-    int sf = ((CustomLR1121 *)_radio)->getSpreadingFactor();
-    return packetScoreInt(snr, sf, packet_len);
+    return ((CustomLR1121*)_radio)->getSpreadingFactor();
   }
 
   void onSendFinished() override {
     RadioLibWrapper::onSendFinished();
-    _radio->setPreambleLength(
-        preambleLengthForSF(getSpreadingFactor()));
+    _radio->setPreambleLength(preambleLengthForSF(getSpreadingFactor()));
   }
 
-  void doResetAGC() override {
-    CustomLR1121 *r = (CustomLR1121 *)_radio;
-    float freq = r->getFreqMHz();
-
-    r->standby(RADIOLIB_LR11X0_STANDBY_RC, true);
-    r->calibrate(RADIOLIB_LR11X0_CALIBRATE_ALL);
-    r->setFrequency(freq);
-    r->setRxBoostedGainMode(r->getRxBoostedGainMode());
-  }
-
+  float getLastRSSI() const override { return ((CustomLR1121 *)_radio)->getRSSI(); }
+  float getLastSNR() const override { return ((CustomLR1121 *)_radio)->getSNR(); }
+  
   void setRxBoostedGainMode(bool en) override {
     ((CustomLR1121 *)_radio)->setRxBoostedGainMode(en);
   }
-
   bool getRxBoostedGainMode() const override {
     return ((CustomLR1121 *)_radio)->getRxBoostedGainMode();
-  }
-
-  void powerOff() override {
-    ((CustomLR1121 *)_radio)->sleep(false, 0);
   }
 };
